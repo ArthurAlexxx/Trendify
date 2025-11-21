813a487bb386b69eb3597e5361f1c9adb353ac96
arthur asd <artvaldivia2014@gmail.com>
2025-11-21 23:29:07 +0000

    🔹 Diagnóstico
    meu bucket real, que existe mesmo, é:
    
    studio-4233590611

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4233590611-a8ab0",
   "appId": "1:145551166878:web:4b62ae63197e30f87e5a8f",
-  "storageBucket": "studio-4233590611-a8ab0.appspot.com",
+  "storageBucket": "studio-4233590611-a8ab0.firebaseapp.com",
   "apiKey": "AIzaSyCMNk91CkJIWDUWWrhgLrqm-OGZKtSiMmw",
   "authDomain": "studio-4233590611-a8ab0.firebaseapp.com",
   "measurementId": "",
