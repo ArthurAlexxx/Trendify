a916f02e6141a8b09b0b4e059f66c179cc49505e
arthur asd <artvaldivia2014@gmail.com>
2025-11-26 02:36:47 +0000

    minha barra lateral em dispositivos maiores nao esta no canto da tela. (

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { AppSidebar } from '@/components/app-sidebar';
@@ -30,26 +29,26 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background">
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 md:pl-64">
-         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 md:hidden">
-          <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
-            <SheetTrigger asChild>
-              <Button size="icon" variant="outline">
-                <PanelLeft className="h-5 w-5" />
-                <span className="sr-only">Toggle Menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="sm:max-w-xs p-0">
-               <AppSidebar isMobile setIsMobileMenuOpen={setIsMobileMenuOpen} />
-            </SheetContent>
-          </Sheet>
-        </header>
-        <AppSidebar />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 w-full">
-          {children}
-        </main>
-      </div>
+    <div className="flex min-h-screen w-full bg-background">
+        <AppSidebar isMobile={false} setIsMobileMenuOpen={setIsMobileMenuOpen} />
+        <div className="flex flex-col sm:gap-4 sm:py-4 w-full md:pl-64">
+            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 md:hidden">
+            <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
+                <SheetTrigger asChild>
+                <Button size="icon" variant="outline">
+                    <PanelLeft className="h-5 w-5" />
+                    <span className="sr-only">Toggle Menu</span>
+                </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="sm:max-w-xs p-0">
+                <AppSidebar isMobile setIsMobileMenuOpen={setIsMobileMenuOpen} />
+                </SheetContent>
+            </Sheet>
+            </header>
+            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 w-full">
+            {children}
+            </main>
+        </div>
     </div>
   );
 }
