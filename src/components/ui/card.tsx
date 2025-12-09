22543f405c12b4ae54b6a7a1142a12c637900ec9
arthur asd <artvaldivia2014@gmail.com>
2025-12-09 04:00:06 +0000

    voce consegue deixar invisivel esses erros do console?
    bundle.js:45 War

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -48,10 +48,10 @@ CardTitle.displayName = "CardTitle"
 
 
 const CardDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <p
+  <div
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
