15eb65712e4e45b9673184c9c913e22eef615fe9
arthur asd <artvaldivia2014@gmail.com>
2025-12-11 07:18:12 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: stat is

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -78,7 +78,7 @@ export const reducer = (state: State, action: Action): State => {
     case "ADD_TOAST":
       return {
         ...state,
-        toasts: [action.toast, ...stat.toasts].slice(0, TOAST_LIMIT),
+        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
       }
 
     case "UPDATE_TOAST":
