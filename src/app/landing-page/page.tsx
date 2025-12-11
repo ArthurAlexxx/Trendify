39dd7da8711e5dd37dc544415edfec5263b45e0a
arthur asd <artvaldivia2014@gmail.com>
2025-12-11 02:01:13 +0000

    legal! tire as bordasem si, e deixe os cards um pouco maior

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -358,7 +358,7 @@ export default function LandingPage() {
 
 				<AnimatedContainer
 					delay={0.4}
-					className="grid grid-cols-1 divide-x divide-y border sm:grid-cols-2 md:grid-cols-3"
+					className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3"
 				>
 					{features.map((feature, i) => (
 						<FeatureCard key={i} feature={feature} />
--- src/components/ui/grid-feature-cards.tsx
+++ src/components/ui/grid-feature-cards.tsx
@@ -2,6 +2,7 @@
 'use client';
 import { cn } from '@/lib/utils';
 import React from 'react';
+import { Card } from './card';
 
 type FeatureType = {
 	title: string;
@@ -10,16 +11,16 @@ type FeatureType = {
     plan: 'pro' | 'premium';
 };
 
-type FeatureCardPorps = React.ComponentProps<'div'> & {
+type FeatureCardPorps = React.ComponentProps<typeof Card> & {
 	feature: FeatureType;
 };
 
 export function FeatureCard({ feature, className, ...props }: FeatureCardPorps) {
 	return (
-		<div className={cn('relative overflow-hidden p-6', className)} {...props}>
+		<Card className={cn('relative overflow-hidden p-8', className)} {...props}>
 			<feature.icon className="text-primary size-6" strokeWidth={1.5} aria-hidden />
 			<h3 className="mt-6 text-sm font-semibold md:text-base">{feature.title}</h3>
 			<p className="text-muted-foreground relative z-20 mt-2 text-xs font-light">{feature.description}</p>
-		</div>
+		</Card>
 	);
 }
